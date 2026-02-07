b4910dec13bd86d8f268dee18bd0c22c246afe78
Amelia Plaku <ameliaplaku13@gmail.com>
2026-02-07 17:56:28 +0000

    I added Music because why not

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
-#this is little itty bitty dude
+"""
 
-player_x = 0
-player_y = 4
-#this sum' obstacles to make his life hard heheahaaaHAHAHAAA
-obstacle_x = 4
+this is little itty bitty dude
 
-#ready ... ?
-basic.show_string("3 2 1")
+"""
+# this is how he jumps
 
-
-#this is how he jumps
 def on_button_pressed_a():
     global player_y
     player_y = 2
     basic.pause(200)
-    music.play(music.string_playable("A B C5 - - - - - - ", 1200),
-       music.PlaybackMode.UNTIL_DONE)
+    music._play_default_background(music.built_in_playable_melody(Melodies.JUMP_UP),
+        music.PlaybackMode.UNTIL_DONE)
     player_y = 4
 input.on_button_pressed(Button.A, on_button_pressed_a)
-
 
 def game_loop():
     global obstacle_x
     while True:
+        player_x = 0
         basic.clear_screen()
         led.plot(player_x, player_y)
         led.plot(obstacle_x, 4)
-
-        #collision check
+        # collision check
         if obstacle_x == player_x and player_y == 4:
             basic.show_string("GAME OVER")
             break
-
-        obstacle_x -= 1
-
+        obstacle_x += 0 - 1
         if obstacle_x < 0:
-            obstacle_x = 4   #reset obstacle
-  
+            obstacle_x = 4
+        # reset obstacle
         basic.pause(200)
-
+obstacle_x = 0
+player_y = 0
+for index in range(4):
+    music._play_default_background(music.built_in_playable_melody(Melodies.NYAN),
+        music.PlaybackMode.LOOPING_IN_BACKGROUND)
+player_y = 4
+# this sum' obstacles to make his life hard heheahaaaHAHAHAAA
+obstacle_x = 4
+# ready ... ?
+basic.show_string("3 2 1")
 game_loop()
--- main.ts
+++ main.ts
@@ -1,38 +1,44 @@
-// this is little itty bitty dude
-let player_x = 0
-let player_y = 4
-// this sum' obstacles to make his life hard heheahaaaHAHAHAAA
-let obstacle_x = 4
-// ready ... ?
-basic.showString("3 2 1")
-// this is how he jumps
+/** this is little itty bitty dude */
+//  this is how he jumps
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
     player_y = 2
     basic.pause(200)
-    music.play(music.stringPlayable("A B C5 - - - - - - ", 1200), music.PlaybackMode.UntilDone)
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.JumpUp), music.PlaybackMode.UntilDone)
     player_y = 4
 })
 function game_loop() {
+    let player_x: number;
     
     while (true) {
+        player_x = 0
         basic.clearScreen()
         led.plot(player_x, player_y)
         led.plot(obstacle_x, 4)
-        // collision check
+        //  collision check
         if (obstacle_x == player_x && player_y == 4) {
             basic.showString("GAME OVER")
             break
         }
         
-        obstacle_x -= 1
+        obstacle_x += 0 - 1
         if (obstacle_x < 0) {
             obstacle_x = 4
         }
         
-        // reset obstacle
+        //  reset obstacle
         basic.pause(200)
     }
 }
 
+let obstacle_x = 0
+let player_y = 0
+for (let index = 0; index < 4; index++) {
+    music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Nyan), music.PlaybackMode.LoopingInBackground)
+}
+player_y = 4
+//  this sum' obstacles to make his life hard heheahaaaHAHAHAAA
+obstacle_x = 4
+//  ready ... ?
+basic.showString("3 2 1")
 game_loop()
